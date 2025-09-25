5c5ffb772f96baf740d20a3904367a595df101a0
Tim Strazzere <tim@strazzere.com>
2025-09-24 23:42:02 -0700

    (fix) ensure patch got works for aarch64

--- hook/src/patch.rs
+++ hook/src/patch.rs
@@ -134,7 +134,13 @@ pub unsafe fn patch_got_entry(addr: *mut *const c_void, replacement: *const c_vo
     mprotect(page as *mut c_void, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
 
     let original = *addr;
-    *addr = (replacement as usize | 1) as *const c_void;
+
+    if cfg!(target_arch = "aarch64") {
+        *addr = replacement;
+    } else {
+        // For 32-bit ARM, set the Thumb bit
+        *addr = (replacement as usize | 1) as *const c_void;
+    }
 
     mprotect(page as *mut c_void, page_size, PROT_READ | PROT_EXEC);
 
